e597336d7a6efeef59027c4e7696796a4103f4ae
Cooper <coop@coopeeo.xyz>
2025-04-05 13:06:45 -0500

    add percents on swap and memory usage

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ fn main() {
     println!("=>System information");
     println!("System name: {}", System::name().unwrap_or_default());
     println!("System kernel version: {}", System::kernel_version().unwrap_or_default());
-    println!("Memory Usage : {}/{} MB", sys.used_memory() / 1024 /1024, sys.total_memory() / 1024 / 1024);
+    println!("Memory Usage : {}/{} MB ({}%)", sys.used_memory() / 1024 /1024, sys.total_memory() / 1024 / 1024, sys.used_memory() * 100 / sys.total_memory());
     if sys.total_swap() != 0 {
-        println!("Swap Usage : {}/{} MB", sys.used_swap() / 1024 /1024, sys.total_swap() / 1024 / 1024);
+        println!("Swap Usage : {}/{} MB ({}%)", sys.used_swap() / 1024 /1024, sys.total_swap() / 1024 / 1024, sys.used_swap() * 100 / sys.total_swap());
     }
     println!("CPU Usage : {}%", sys.global_cpu_usage());
 
