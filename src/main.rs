0031ffbf7323c4b5fa05edd4d6de6f3893390c33
BuildTools <unconfigured@null.spigotmc.org>
2025-05-03 21:53:18 -0500

    Added More Coloring, Fixed Fedora Ascii(replacement wanted)

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,14 @@ fn ascii_art() -> [String; 8] {
         retval[7] = String::from(r"    /.^         ^.\ ");
     }
     else if os_version.contains("Fedora"){
-        retval[0] = String::from(r"           .        ");
-        retval[1] = String::from(r"          / \       ");
-        retval[2] = String::from(r"         /   \      ");
-        retval[3] = String::from(r"        /^.   \     ");
-        retval[4] = String::from(r"       /  .-.  \    ");
-        retval[5] = String::from(r"      /  (   ) _\   ");
-        retval[6] = String::from(r"     / _.~   ~._^\  ");
-        retval[7] = String::from(r"    /.^         ^.\ ");
+        retval[0] = String::from(r"    ▓▓▓▓▓▓▓▓▓▓▓▓    ");
+        retval[1] = String::from(r"  ▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓  ");
+        retval[2] = String::from(r" ▓▓▓▓▓▓▓▓ ░▓▓  ▓▓▓▓ ");
+        retval[3] = String::from(r"▒▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓ ");
+        retval[4] = String::from(r"▓▓▓▓    ▓   ▓▓▓▓▓▓▓ ");
+        retval[5] = String::from(r"▓▓▓  ▓▓▓▓ ▓▓▓▓▓▓▓▓▓ ");
+        retval[6] = String::from(r"▓▓▓▓  ▓▒  ▓▓▓▓▓▓▓▓  ");
+        retval[7] = String::from(r"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ");
     }
      else if os_version.contains("Windows"){
 
@@ -49,21 +49,22 @@ fn main() {
     print!("{}",os_ascii[1].bright_cyan());
     println!("System name: {}", System::name().unwrap().cyan());
     print!("{}",os_ascii[2].bright_cyan());
-    println!("Operating System Version: {}", System::long_os_version().unwrap_or_default().red());
+    println!("Operating System Version: {}", System::long_os_version().unwrap_or_default().cyan());
     print!("{}",os_ascii[3].bright_cyan());
-    println!("System kernel version: {}", System::kernel_long_version());
+    println!("System kernel version: {}", System::kernel_long_version().cyan());
     print!("{}",os_ascii[4].bright_cyan());
-    println!("Memory Usage : {}/{} MB ({}%)", sys.used_memory() / 1024 /1024, sys.total_memory() / 1024 / 1024, sys.used_memory() * 100 / sys.total_memory());
+    println!("Hostname: {}", System::host_name().unwrap().cyan());
     print!("{}",os_ascii[5].bright_cyan());
+    println!("Memory Usage : {}/{} MB ({}%)", sys.used_memory() / 1024 /1024, sys.total_memory() / 1024 / 1024, sys.used_memory() * 100 / sys.total_memory());
+    print!("{}",os_ascii[6].bright_cyan());
     if sys.total_swap() != 0 {
         println!("Swap Usage : {}/{} MB ({}%)", sys.used_swap() / 1024 /1024, sys.total_swap() / 1024 / 1024, sys.used_swap() * 100 / sys.total_swap());
     }
     else {
         println!("No swap memory available"); 
     }
-    print!("{}",os_ascii[6].bright_cyan());
+    print!("{}",os_ascii[7].bright_cyan());
     println!("CPU Usage : {}%", sys.global_cpu_usage());
-    println!("{}",os_ascii[7].bright_cyan());
     // println!("CPU Temperature : {}°C", sys.cpu_temperature().unwrap_or_default());
     // println!("Battery Usage: {}%", sys.used_battery())
     
