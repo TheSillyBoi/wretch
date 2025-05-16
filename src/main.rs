82de0d982b291280809c73ed5b7d2b79358e1f33
Adrian Tennies <adrian@tennies.us>
2025-05-15 22:16:48 -0500

    Added an extra file for ASCII arts to improve readability

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,16 @@
 use clap::Parser; // Import the Parser trait from clap
 use sysinfo::System; // Import the System struct from sysinfo
-use colored::{Colorize,ColoredString}; //import the Colorize trait and ColoredString struct from colored
+use colored::Colorize; //import the Colorize trait and ColoredString struct from colored
+mod ascii; // Import the ascii module
 
-
-fn ascii_art() -> [ColoredString; 8] { // Function to Select an ASCII art based on the OS
-    let os_version = System::long_os_version().unwrap_or_default().to_lowercase(); // Get the OS version and convert it to lowercase
-    let mut retval: [ColoredString; 8] = Default::default(); // Initialize an array of ColoredString with 8 elements
-    if os_version.contains("arch"){ // if the OS version contains "arch"(meant to be used for arch linux)
-        retval[0] = ColoredString::from(r"           .        ").bright_cyan();
-        retval[1] = ColoredString::from(r"          / \       ").bright_cyan();
-        retval[2] = ColoredString::from(r"         /   \      ").bright_cyan();
-        retval[3] = ColoredString::from(r"        /^.   \     ").bright_cyan();
-        retval[4] = ColoredString::from(r"       /  .-.  \    ").bright_cyan();
-        retval[5] = ColoredString::from(r"      /  (   ) _\   ").bright_cyan();
-        retval[6] = ColoredString::from(r"     / _.~   ~._^\  ").bright_cyan();
-        retval[7] = ColoredString::from(r"    /.^         ^.\ ").bright_cyan();
-    } else if os_version.contains("fedora"){ // if the OS version contains "fedora" meant for fedora linux
-        retval[0] = ColoredString::from(r"    ╔══════════╗    ").bright_cyan();
-        retval[1] = ColoredString::from(r"  ╔═╝          ╚═╗  ").bright_cyan();
-        retval[2] = ColoredString::from(r" ╔╝     ╔═══╗    ╚╗ ").bright_cyan();
-        retval[3] = ColoredString::from(r"╔╝      ║         ║ ").bright_cyan();
-        retval[4] = ColoredString::from(r"║  ╔═══ ╠═══      ║ ").bright_cyan();
-        retval[5] = ColoredString::from(r"║  ║    ║        ╔╝ ").bright_cyan();
-        retval[6] = ColoredString::from(r"║  ╚════╝      ╔═╝  ").bright_cyan();
-        retval[7] = ColoredString::from(r"╚══════════════╝    ").bright_cyan();
-    } else if os_version.contains("windows"){ // if the OS version contains "windows" meant for windows
-        retval[0] = ColoredString::from(r" ╔══════╗  ╔══════╗ ").truecolor(0, 120, 212);
-        retval[1] = ColoredString::from(r" ║      ║  ║      ║ ").truecolor(0, 120, 212);
-        retval[2] = ColoredString::from(r" ║      ║  ║      ║ ").truecolor(0, 120, 212);
-        retval[3] = ColoredString::from(r" ╚══════╝  ╚══════╝ ").truecolor(0, 120, 212);
-        retval[4] = ColoredString::from(r" ╔══════╗  ╔══════╗ ").truecolor(0, 120, 212);
-        retval[5] = ColoredString::from(r" ║      ║  ║      ║ ").truecolor(0, 120, 212);
-        retval[6] = ColoredString::from(r" ║      ║  ║      ║ ").truecolor(0, 120, 212);
-        retval[7] = ColoredString::from(r" ╚══════╝  ╚══════╝ ").truecolor(0, 120, 212);
-    } else if os_version.contains("mac"){ // if the OS version contains "mac" meant for macOS
-        retval[0] = ColoredString::from(r"           ╔═       ").white();
-        retval[1] = ColoredString::from(r"          ╔╝        ").white();
-        retval[2] = ColoredString::from(r"   ╔════╗   ╔═══╗   ").white();
-        retval[3] = ColoredString::from(r"  ╔╝    ╚═══╝  ╔╝   ").white();
-        retval[4] = ColoredString::from(r"  ║           ╔╝    ").white();
-        retval[5] = ColoredString::from(r"  ║           ╚╗    ").white();
-        retval[6] = ColoredString::from(r"  ╚╗   ╔════╗  ╚╗   ").white();
-        retval[7] = ColoredString::from(r"   ╚═══╝    ╚═══╝   ").white();
-    } else if os_version.contains("debian"){ // if the OS version contains "debian" meant for debian linux, to do
-        retval[0] = ColoredString::from(r"        ╔═════╗     ").red();
-        retval[1] = ColoredString::from(r"      ╔═╝╔══╗ ║     ").red();
-        retval[2] = ColoredString::from(r"     ╔╝  ║ ╚╝╔╝     ").red();  
-        retval[3] = ColoredString::from(r"     ║   ╚═══╝      ").red(); 
-        retval[4] = ColoredString::from(r"     ╚╗             ").red(); 
-        retval[5] = ColoredString::from(r"      ╚═╗           ").red(); 
-        retval[6] = ColoredString::from(r"        ╚═╗         ").red(); 
-        retval[7] = ColoredString::from(r"          ╚═        ").red();
-    } else if os_version.contains("void"){ // if the OS version contains "void" meant for void linux
-        retval[0] = ColoredString::from(r"       ╔═══════╗    ").green();
-        retval[1] = ColoredString::from(r"       ╚═════╗ ╚═╗  ").green();
-        retval[2] = ColoredString::from(r"             ╚═╗ ║  ").green();
-        retval[3] = ColoredString::from(r"   ╔═╗   ╔═╗   ║ ║  ").green();
-        retval[4] = ColoredString::from(r"   ║ ║   ╚═╝   ╚═╝  ").green();
-        retval[5] = ColoredString::from(r"   ║ ╚═╗            ").green();
-        retval[6] = ColoredString::from(r"   ╚═╗ ╚═════╗      ").green();
-        retval[7] = ColoredString::from(r"     ╚═══════╝      ").green();
-    } else if os_version.contains("suse"){ // if the OS version contains "suse" meant for opensuse linux
-        retval[0] = ColoredString::from(r"      ╔═══════╗     ").green();
-        retval[1] = ColoredString::from(r"    ╔═╝╔══╗   ╚═╗   ").green();
-        retval[2] = ColoredString::from(r"  ╔═╩══╝  ╚═══╗ ╚═╗ ").green();
-        retval[3] = ColoredString::from(r"  ║        ╔═╗╚╗  ║ ").green();
-        retval[4] = ColoredString::from(r"  ║        ╚═╝╔╝  ║ ").green();
-        retval[5] = ColoredString::from(r"  ╚═╗   ╔═════╝ ╔═╝ ").green();
-        retval[6] = ColoredString::from(r"    ╚═╦═╩════ ╔═╝   ").green();
-        retval[7] = ColoredString::from(r"      ╚═══════╝     ").green();
-    } else if os_version.contains("ubuntu"){ // if the OS version contains "ubuntu" meant for ubuntu linux
-        retval[0] = ColoredString::from(r"             ╔═╗    ").truecolor(250, 70, 22);
-        retval[1] = ColoredString::from(r"             ╚═╝    ").truecolor(250, 70, 22);
-        retval[2] = ColoredString::from(r"    ╔══   ════╗     ").truecolor(250, 70, 22);
-        retval[3] = ColoredString::from(r"╔═╗ ║  ╔════╗ ║     ").truecolor(250, 70, 22);
-        retval[4] = ColoredString::from(r"╚═╝ ║  ║    ║ ║     ").truecolor(250, 70, 22);
-        retval[5] = ColoredString::from(r"    ║  ╚════╝       ").truecolor(250, 70, 22);
-        retval[6] = ColoredString::from(r"      ════════╝ ╔═╗ ").truecolor(250, 70, 22);
-        retval[7] = ColoredString::from(r"                ╚═╝ ").truecolor(250, 70, 22);
-    } else if os_version.contains("zorin"){ // if the OS version contains "zorin" meant for zorinOS linux
-        retval[0] = ColoredString::from(r"    ____________    ").blue();
-        retval[1] = ColoredString::from(r"   /____________\   ").blue();
-        retval[2] = ColoredString::from(r"  _______           ").blue();
-        retval[3] = ColoredString::from(r" / _____/   _____/\ ").blue();
-        retval[4] = ColoredString::from(r" \/        /______/ ").blue();
-        retval[5] = ColoredString::from(r"   ______________   ").blue();
-        retval[6] = ColoredString::from(r"   \____________/   ").blue();
-        retval[7] = ColoredString::from(r"                    ").blue();
-    } else if os_version.contains("linux"){ // if the OS version contains "linux" meant for other linux distros
-        retval[0] = ColoredString::from(r"        .---.       ");
-        retval[1] = ColoredString::from(r"       /     \      ");
-        retval[2] = ColoredString::from(r"       \.o-o./      ");
-        retval[3] = ColoredString::from(r"       /`\_/`\      ");
-        retval[4] = ColoredString::from(r"      //     \\     ");
-        retval[5] = ColoredString::from(r"     | \     )|_    ");
-        retval[6] = ColoredString::from(r"    /`\_`>  <_/ \   ");
-        retval[7] = ColoredString::from(r" jgs\__/'---'\__/   ");
-    } else { // if the OS version does not match any of the above
-        retval[0] = ColoredString::from(r"╔══════════════════╗").truecolor(255,255,255);
-        retval[1] = ColoredString::from(r"║                  ║").truecolor(255,255,255);
-        retval[2] = ColoredString::from(r"║                  ║").truecolor(255,255,255);
-        retval[3] = ColoredString::from(r"║                  ║").truecolor(255,255,255);
-        retval[4] = ColoredString::from(r"╚═══════╦══╦═══════╝").truecolor(255,255,255);
-        retval[5] = ColoredString::from(r"        ║  ║        ").truecolor(255,255,255);
-        retval[6] = ColoredString::from(r"    ╔═══╝  ╚═══╗    ").truecolor(255,255,255);
-        retval[7] = ColoredString::from(r"    ╚══════════╝    ").truecolor(255,255,255);
-      
-    }
-
-    retval
-}
 
 
 #[derive(Parser, Debug)]
 #[command(name = "wretch", version = build_version(), about = "the tool to get information about your system", ignore_errors(true))]
 struct Args {}
-
 fn main() {
-    let os_ascii = ascii_art(); // Calls the ascii_art function to get the ASCII art based on the OS
+//    mod ascii;
+    let os_ascii = ascii::ascii_art(); // Calls the ascii_art function to get the ASCII art based on the OS
     let mut sys = System::new_all(); // Gather system information
     sys.refresh_all(); // Refresh all system information
 
@@ -158,7 +51,7 @@ fn main() {
     }
 
     print!("{}", os_ascii[7]);// prints the eighth line of the ascii art
-    println!("CPU Usage: {}", format!("{}%", sys.global_cpu_usage().to_string()).cyan()); // prints the CPU usage as a percentage
+    println!("CPU Usage: {}", format!("{}%", sys.global_cpu_usage()).cyan()); // prints the CPU usage as a percentage
 }
 
 
@@ -181,11 +74,9 @@ fn format_version() -> String {
         } else {
             format!("{base}-nightly (commit unknown)")
         }
-    } else {
-        if commit != "" {
+    } else if commit != "" {
             format!("{base} ({commit})")
-        } else {
+    } else {
             format!("{base} (commit unknown)")
         }
-    }
 }
