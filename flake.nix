9f330c67108f34906b13869a5d1516458c5c6196
udontur <h@hadrian.cc>
2025-05-30 17:44:36 +0800

    update: nix flakes

--- flake.nix
+++ flake.nix
@@ -3,30 +3,40 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   outputs =
     { self, nixpkgs }:
-    let 
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in{
-      packages.x86_64-linux.default =
-        with import nixpkgs {
-          system = "x86_64-linux";
-        };
-        pkgs.rustPlatform.buildRustPackage rec {
-          pname = "wretch";
-          version = "nightly";
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "wretch";
+            version = "nightly";
 
-          src = self;
+            src = self;
 
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            nativeBuildInputs = with pkgs; [
+              cargo
+              rustc
+              pkgs.pkg-config
+              git
+            ];
           };
-
-          nativeBuildInputs = with pkgs;[
-            cargo
-            rustc
-            pkgs.pkg-config
-            git
-          ];
-        };
+        }
+      );
     };
 }
